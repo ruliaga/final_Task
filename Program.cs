b08ab12993e7ca542b9b2607d62176832ff7babc
[Ruliaga] <ruliaga@mail.ru>
2022-10-01 16:26:47 +0500

    Program.cs code is done

--- Program.cs
+++ Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+Console.WriteLine("Введите через пробел элементы массива строк:");
+string phrase =Console.ReadLine();
+string[] strings=phrase.Split(' ');
+
+void PrintArray (string[] array){
+Console.Write("[");
+foreach (var word in array){
+Console.Write($"'{word}' ");}
+}
+PrintArray(strings);
+Console.Write("] -> ");
+PrintArray(NewArray(strings));
+Console.Write("]");
+
+string [] NewArray (string[] strings){
+   
+Random rnd=new Random();
+string [] newArray = new string[rnd.Next(0,3)];
+for (int i=0; i<newArray.Length;i++){
+    newArray[i]=strings[rnd.Next(0,strings.Length)];
+}
+return newArray;
+}
+
